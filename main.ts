15a3f05381661d704552ab6df126d14bf46b5f31
AJ <65269484+aiden-ji@users.noreply.github.com>
2023-08-05 15:12:08 -0700

    button speed control

--- main.ts
+++ main.ts
@@ -1,19 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    speed += -10
+})
+input.onButtonPressed(Button.B, function () {
+    speed += 10
+})
 let add = 0
 let count = 0
 let speed = 13
-basic.forever(function on_forever() {
-    
-    if (count == 0) {
-        add = 1
-    }
-    
-    if (count == 4) {
-        add = -1
-    }
-    
+basic.forever(function () {
     led.plot(count, 2)
     basic.pause(speed)
     basic.clearScreen()
     count += add
     basic.pause(speed)
+})
+basic.forever(function () {
+    if (count == 0) {
+        add = 1
+    }
+    if (count == 4) {
+        add = -1
+    }
 })
